c0f5ca70f044f9c6d87a4f56ff7011f40062a233
MHD-X <mohamed.altaeeb.5h243@gmail.com>
2025-05-25 18:47:54 +0000

    ddd

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-xh = input("Enter a number: ")
-xr = input("Enter a number: ")
-xp = float(xh) * float(xr)
-print(xp)
-if xp > 100:
-    print("xp is greater than 100")  # this is a comment
+# قراءة عدد الساعات والأجر لكل ساعة من المستخدم
+xh = input('Enter hours: ')
+xr = input('Enter rate per hour: ')
+
+# تحويل إلى أعداد عشرية
+fh = float(xh)
+fr = float(xr)
+
+# حساب الأجر
+if fh <= 35:
+  pay_total = fh * fr
+elif fh <= 50:
+  regular_pay = 35 * fr
+  overtime_hours = fh - 35
+  overtime_pay = overtime_hours * fr * 1.25
+  pay_total = regular_pay + overtime_pay
+else:
+  regular_pay = 35 * fr
+  mid_overtime = 15 * fr * 1.25  # من 36 إلى 50
+  high_overtime = (fh - 50) * fr * 1.5
+  pay_total = regular_pay + mid_overtime + high_overtime
+
+# طباعة الناتج
+print("الأجر الإجمالي هو:", pay_total)
--- my_folder/main.py
+++
@@ -1,4 +0,0 @@
-xh = input("Enter a number: ")
-xr = input("Enter a number: ")
-xp = float(xh) * float(xr)
-print(xp)
